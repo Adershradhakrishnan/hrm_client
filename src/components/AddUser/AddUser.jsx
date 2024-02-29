7d3e9ea743a89b712c4b75086658ff01e82c1ccb
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-02-29 09:54:42 +0530

    sswrt message

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -8,12 +8,39 @@ function AddUser(){
     const [pincode,setPincode] = useState('');
     const [password,setPassword] = useState('');
 
+    const handleAddUser = async (e) => {
+        try{
+
+            const data = {name,email,phonenumber,pincode,password};
+            const json_data = JSON.stringify(data);
+            console.log("json_data: ",json_data)
+
+            const response = await fetch('http://localhost:3000/adduser',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: json_data,
+            });
+            console.log('Response received',response);
+
+            if (response.data.success){
+                alert(response.data.message);
+            }else{
+                alert(response.data.message);
+            }
+        } catch(error) {
+            console.log('Adding user failed:',error);
+            alert('failed.please try again later.')
+        }
+    }
+
     return(
 
         <div className="adddata">
             <h2>Add-User</h2>
 
-            <form className="adddetails">
+            <form className="adddetails" onSubmit={handleAddUser}>
                 <label htmlFor="name">Enter Your UserName</label>
                 <input type="text" placeholder="Username" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
                 <label htmlFor="email">Enter Your Email</label>
